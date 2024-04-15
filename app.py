ed32a03f41fc7d44f7f2b9d4c7545cf1cac04b1c
aditigaikwad97 <101386653+aditigaikwad97@users.noreply.github.com>
2024-04-15 16:07:12 +1200

    Add sidebar app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from sklearn.preprocessing import LabelEncoder
-import streamlit
 from predict_page import show_predict_page
+import streamlit as st
 
+st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
 show_predict_page()
+
+
