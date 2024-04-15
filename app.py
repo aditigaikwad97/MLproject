16e5e598e7d2a194e2aafe4b788c02855f7f1c86
aditigaikwad97 <101386653+aditigaikwad97@users.noreply.github.com>
2024-04-15 17:03:12 +1200

    add explore page app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from sklearn.preprocessing import LabelEncoder
 from predict_page import show_predict_page
+from explore_page import show_explore_page
 import streamlit as st
 
-st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
-show_predict_page()
+page = st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
 
 
