4252f55314fdab89d3fb6f742f9bfe73f23b2906
aditigaikwad97 <101386653+aditigaikwad97@users.noreply.github.com>
2024-04-16 12:53:45 +1200

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -103,9 +103,8 @@ def show_explore_page():
         var = r'''${ \large Top \large 10 \large Countries \large Participated \large In \large Survey }$ '''
         st.write(f":blue[{var}]")
         label = r'''$\textsf{
-            \Huge Text \huge Text \LARGE Text \Large Text 
-            \large Text \normalsize Text \small Text 
-            \footnotesize Text \scriptsize Text \tiny Text }$'''
+            \Large Top \Large 10 \Large Countries 
+            \Large Participated \Large In \Large Survey  }$'''
         st.write(label)
     
     
